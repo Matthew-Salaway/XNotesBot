5025a235b07ecc8d16312a139dd0265076212e5c
Abiola David <davidolanrewaju2000@gmail.com>
2024-03-28 02:01:04 +0100

    Set up reply mentions

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ import os
 
 load_dotenv()
 
-CONSUMER_KEY = os.getenv('CONSUMER_KEY')
-CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
-ACCESS_KEY = os.getenv('ACCESS_KEY')
-ACCESS_SECRET = os.getenv('ACCESS_SECRET')
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+TWITTER_ID = os.getenv('TWITTER_ID')
 
-auth = tweepy.OAuth1UserHandler(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+client = tweepy.Client(
+    bearer_token=BEARER_TOKEN,
+    consumer_key=API_KEY,
+    consumer_secret=API_SECRET,
+    access_token=ACCESS_TOKEN,
+    access_token_secret=ACCESS_TOKEN_SECRET
+)
+
+auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 # TODO: Store the id of the most recent responded tweet, which is then used when responding to newer mentions
@@ -26,21 +38,19 @@ def reply_tweets():
         """
     global last_responded_tweet_id
 
-    mentions = api.mentions_timeline(count=30, since_id=last_responded_tweet_id)
+    mentions = client.get_users_mentions(id=TWITTER_ID)
+    last_responded_tweet_id = mentions.meta.get("newest_id", "")  # handle case of missing/empty data
 
-    for mention in reversed(mentions):
+    for mention in reversed(mentions.data):
         # Extract the ID of the mention
         mention_id = mention.id
         # Generate your reply based on the mention
-        reply_text ="Thanks for the mention!"
+        reply_text = "Hi there, thanks for the mention!"
         # Reply to the mention
-        api.update_status(status=reply_text, in_reply_to_status_id=mention_id)
-
-        # Update the last responded tweet ID
-        last_responded_tweet_id = mention_id
+        client.create_tweet(in_reply_to_tweet_id=mention_id, text=reply_text)
 
     return last_responded_tweet_id
 
-if __name__=="__main__":
-    reply_tweets()
 
+if __name__ == "__main__":
+    reply_tweets()
